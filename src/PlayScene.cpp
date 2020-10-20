c1f834e2f14d6c7770f98b9aaca52a1b13722ef5
Andrew Boulanger <andrew_boulanger14@hotmail.com>
2020-10-20 01:33:00 -0400

    Add project files.

---
+++ src/PlayScene.cpp
@@ -0,0 +1,194 @@
+#include "PlayScene.h"
+#include "Game.h"
+#include "EventManager.h"
+
+// required for IMGUI
+#include "imgui.h"
+#include "imgui_sdl.h"
+#include "Renderer.h"
+
+PlayScene::PlayScene()
+{
+	PlayScene::start();
+}
+
+PlayScene::~PlayScene()
+= default;
+
+void PlayScene::draw()
+{
+	TextureManager::Instance()->draw("background", 0, 0, 0, 255, true);
+	if(EventManager::Instance().isIMGUIActive())
+	{
+		GUI_Function();
+	}
+
+	drawDisplayList();
+	SDL_SetRenderDrawColor(Renderer::Instance()->getRenderer(),0,0,0,0);
+
+	SDL_RenderDrawLineF(Renderer::Instance()->getRenderer(), 100, 300, 100, 300 - rise);
+	SDL_RenderDrawLineF(Renderer::Instance()->getRenderer(), 100, 300, 100 + run, 300 );
+	SDL_RenderDrawLineF(Renderer::Instance()->getRenderer(), 100, 300 - rise, 100 + run, 300 );
+}
+
+void PlayScene::update()
+{
+	updateDisplayList();
+}
+
+void PlayScene::clean()
+{
+	removeAllChildren();
+}
+
+void PlayScene::handleEvents()
+{
+	EventManager::Instance().update();
+
+	// handle player movement with GameController
+	if (SDL_NumJoysticks() > 0)
+	{
+		if (EventManager::Instance().getGameController(0) != nullptr)
+		{
+			const auto deadZone = 10000;
+			if (EventManager::Instance().getGameController(0)->LEFT_STICK_X > deadZone)
+			{
+				
+			}
+			else if (EventManager::Instance().getGameController(0)->LEFT_STICK_X < -deadZone)
+			{
+			
+			}
+			else
+			{
+			
+			}
+		}
+	}
+
+
+	// handle player movement if no Game Controllers found
+	if (SDL_NumJoysticks() < 1)
+	{
+		if (EventManager::Instance().isKeyDown(SDL_SCANCODE_A))
+		{
+		
+		}
+		else if (EventManager::Instance().isKeyDown(SDL_SCANCODE_D))
+		{
+		
+		}
+		else
+		{
+		
+		}
+	}
+	
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_ESCAPE))
+	{
+		TheGame::Instance()->quit();
+	}
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_1))
+	{
+		TheGame::Instance()->changeSceneState(START_SCENE);
+	}
+
+	if (EventManager::Instance().isKeyDown(SDL_SCANCODE_2))
+	{
+		TheGame::Instance()->changeSceneState(END_SCENE);
+	}
+}
+
+void PlayScene::start()
+{
+	TextureManager::Instance()->load("../Assets/textures/background.jpg", "background");
+	// Set GUI Title
+	m_guiTitle = "Play Scene";
+	
+	rise = run = 100;
+
+	// Back Button
+	m_pBackButton = new Button("../Assets/textures/backButton.png", "backButton", BACK_BUTTON);
+	m_pBackButton->getTransform()->position = glm::vec2(300.0f, 400.0f);
+	m_pBackButton->addEventListener(CLICK, [&]()-> void
+	{
+		m_pBackButton->setActive(false);
+		TheGame::Instance()->changeSceneState(START_SCENE);
+	});
+
+	m_pBackButton->addEventListener(MOUSE_OVER, [&]()->void
+	{
+		m_pBackButton->setAlpha(128);
+	});
+
+	m_pBackButton->addEventListener(MOUSE_OUT, [&]()->void
+	{
+		m_pBackButton->setAlpha(255);
+	});
+	addChild(m_pBackButton);
+
+	// Next Button
+	m_pNextButton = new Button("../Assets/textures/nextButton.png", "nextButton", NEXT_BUTTON);
+	m_pNextButton->getTransform()->position = glm::vec2(500.0f, 400.0f);
+	m_pNextButton->addEventListener(CLICK, [&]()-> void
+	{
+		m_pNextButton->setActive(false);
+		TheGame::Instance()->changeSceneState(END_SCENE);
+	});
+
+	m_pNextButton->addEventListener(MOUSE_OVER, [&]()->void
+	{
+		m_pNextButton->setAlpha(128);
+	});
+
+	m_pNextButton->addEventListener(MOUSE_OUT, [&]()->void
+	{
+		m_pNextButton->setAlpha(255);
+	});
+
+	addChild(m_pNextButton);
+
+	/* Instructions Label */
+	m_pInstructionsLabel = new Label("Press the backtick (`) character to toggle Debug View", "Consolas");
+	m_pInstructionsLabel->getTransform()->position = glm::vec2(Config::SCREEN_WIDTH * 0.5f, 500.0f);
+
+	addChild(m_pInstructionsLabel);
+}
+
+void PlayScene::GUI_Function() const
+{
+	
+	// Always open with a NewFrame
+	ImGui::NewFrame();
+
+	// See examples by uncommenting the following - also look at imgui_demo.cpp in the IMGUI filter
+	//ImGui::ShowDemoWindow();
+	
+	ImGui::Begin("Your Window Title Goes Here", NULL, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_MenuBar);
+
+	if(ImGui::Button("My Button"))
+	{
+		std::cout << "My Button Pressed" << std::endl;
+	}
+
+	ImGui::Separator();
+
+	static int height = 100;
+	if (ImGui::SliderInt("Height", &height, 1, 500)) {
+		(int)rise = height;
+	}
+	static int length = 100;
+	if (ImGui::SliderInt("Length", &length, 1, 500)) {
+		(int)run = length;
+	}
+	
+	
+	ImGui::End();
+
+	// Don't Remove this
+	ImGui::Render();
+	ImGuiSDL::Render(ImGui::GetDrawData());
+	ImGui::StyleColorsDark();
+}
